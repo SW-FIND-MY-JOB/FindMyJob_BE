5a3aa8a63e3ada8f152efe9c8d787b8c8d8c69f5
KimJooyoung <kimjy2000117@gmail.com>
2025-05-14 02:40:10 +0900

    🎉 AuthService 프로젝트 생성

---
+++ apps/authService/src/main/java/com/example/authservice/HealthController.java
@@ -0,0 +1,17 @@
+package com.example.authservice;
+
+import org.hibernate.cfg.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health-check")
+public class HealthController {
+//    private final Environment env;
+//
+//    public HealthController(Environment env) {
+//        this.env = env;
+//    }
+
+
+}
