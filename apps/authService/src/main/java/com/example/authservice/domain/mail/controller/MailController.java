27466db7bee7b92ae511a05066918ac8b5f2a346
KimJooyoung <kimjy2000117@gmail.com>
2025-05-22 15:31:29 +0900

    🔧 Swagger 설명 추가

--- apps/authService/src/main/java/com/example/authservice/domain/mail/controller/MailController.java
+++ apps/authService/src/main/java/com/example/authservice/domain/mail/controller/MailController.java
@@ -4,6 +4,8 @@ import com.example.authservice.domain.mail.dto.MailReqDTO;
 import com.example.authservice.domain.mail.exception.status.MailSuccessStatus;
 import com.example.authservice.domain.mail.service.MailService;
 import com.example.responselib.apiPayload.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
 import jakarta.validation.constraints.Null;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/mail")
+@Tag(name = "메일 인증 관련 API입니다", description = "메일 인증 관련 API입니다")
 public class MailController {
     private final MailService mailService;
 
     //회원가입 인증번호 발송
     @PostMapping("/auth-code")
+    @Operation(summary = "회원가입 인증 발송", description = "회원가입 인증 발송")
     public ApiResponse<Null> sendAuthCode(@RequestBody MailReqDTO.MailDTO mailDTO) throws MessagingException {
         mailService.sendMail(mailDTO);
 
@@ -28,6 +32,7 @@ public class MailController {
 
     //인증번호 검증
     @PostMapping("/verify-code")
+    @Operation(summary = "인증번호 검증", description = "인증번호 검증")
     public ApiResponse<Null> verifyAuthCode(@RequestBody MailReqDTO.VerifyCodeDTO verifyCodeDTO){
         mailService.verifyEmailCode(verifyCodeDTO);
 
--- apps/authService/src/main/java/com/example/authservice/global/jwt/controller/ReissueController.java
+++ apps/authService/src/main/java/com/example/authservice/global/jwt/controller/ReissueController.java
@@ -3,6 +3,8 @@ package com.example.authservice.global.jwt.controller;
 import com.example.authservice.global.jwt.exception.status.ReissueSuccessStatus;
 import com.example.authservice.global.jwt.service.ReissueService;
 import com.example.responselib.apiPayload.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.Null;
@@ -14,10 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/token")
+@Tag(name = "리프레시 토큰 API입니다", description = "리프레시 토큰 관련 API입니다")
 public class ReissueController {
     private final ReissueService reissueService;
 
     @PostMapping("/reissue")
+    @Operation(summary = "토큰 재발급", description = "토큰 재발급")
     public ApiResponse<Null> reissue(HttpServletRequest request, HttpServletResponse response){
         reissueService.reissueToken(request, response);
 
