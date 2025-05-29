af977e6589519d158d7469e535e0fccf52c619b2
KimJooyoung <kimjy2000117@gmail.com>
2025-05-29 11:33:53 +0900

    🔧 Cors 설정 수정

--- apps/apiGateWay/src/main/java/com/example/apigateway/global/config/CorsConfig.java
+++ apps/apiGateWay/src/main/java/com/example/apigateway/global/config/CorsConfig.java
@@ -14,12 +14,10 @@ public class CorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
-
         config.setAllowCredentials(true); // 쿠키, 인증정보 포함 여부
-        config.addAllowedOriginPattern("*"); // 모든 도메인 허용
+        config.setAllowedOriginPatterns(List.of("http://localhost:5173", "https://findmyjob.net", "https://www.findmyjob.net"));
         config.addAllowedHeader("*"); // 모든 헤더 허용
         config.addAllowedMethod("*"); // GET, POST, PUT, DELETE 등 모든 메서드 허용
-//        config.setAllowedOriginPatterns(List.of("https://your-frontend.com", "http://localhost:5173"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
         source.registerCorsConfiguration("/**", config); // 모든 경로에 대해 위 CORS 적용
